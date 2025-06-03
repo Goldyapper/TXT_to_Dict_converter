c20735b7b160db62b03818e04ee5185d0c2ae6d0
Adam Parsons <adam.a.parsons15@gmail.com>
2025-06-03 11:13:46 +0100

    changed it to do a slightly different thing

--- txt to dict converter.py
+++ txt to dict converter.py
@@ -3,8 +3,11 @@ import json
 lines = {}
 with open('tube_stop_info.txt', 'r') as file:
     for line in file:
-        key, value = line.strip().split('->', 1)
-        lines[key.strip()] = value.strip()
+        parts = line.strip().split(' ', 1)
+        if len(parts) == 2:
+            code, name = parts
+            name = name.replace(' Underground Station', '')
+            lines[name] = code
 
 print(json.dumps(lines,indent=1))
 
